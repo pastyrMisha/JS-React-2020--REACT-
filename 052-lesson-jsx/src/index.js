b9ea4d7d41b57a676f5a220fa4cd7a61eafccd95
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-07-09 11:32:00 +0300

    start

--- 052-lesson-jsx/src/index.js
+++ 052-lesson-jsx/src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const elem = (
+//   <div>
+//     <h2>Hello World!</h2>
+//     <input type="text" placeholder="Type here" />
+//     <button/> 
+//   </div>
+// )
+
+const Header = () => {
+  return <h2>Hello World!</h2>
+}
+
+const Field = () => {
+  return <input type="text" placeholder="Type here" />
+}
+
+const Btn = () => {
+  return <button/>
+}
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+      <Field/>
+      <Btn/>
+    </div>
+  )
+}
+
+
+
+// const elem = React.createElement('h2', null, "Hello World!!!")
+
+ReactDOM.render(<App/>, document.getElementById('root'));
