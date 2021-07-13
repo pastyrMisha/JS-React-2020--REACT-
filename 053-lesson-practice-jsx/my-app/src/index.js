4dd3939e7863797155c51afca465589277f1cff2
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-07-13 13:50:26 +0300

    start

--- 053-lesson-practice-jsx/my-app/src/index.js
+++ 053-lesson-practice-jsx/my-app/src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/app/';
+// import App from './components/app/';
+
+function WhoAmI() {
+  return (
+    <React.Fragment>
+      <h1>My name is, surname — </h1>
+      <a href="">My profile</a>
+    </React.Fragment>
+  )
+}
 
 
 ReactDOM.render(
