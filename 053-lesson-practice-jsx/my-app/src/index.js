fd914f33813838b3006860ab48cedae3d7d1bdf9
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-07-18 22:56:14 +0300

    welldone

--- 053-lesson-practice-jsx/my-app/src/index.js
+++ 053-lesson-practice-jsx/my-app/src/index.js
@@ -1,22 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './components/app/';
+import App from './components/app/';
 
-function WhoAmI(props) {
-  return (
-    <>
-      
-      <h1>My name is {props.name}, surname — {props.surname}</h1>
-      <a href="{props.link}">My profile</a>
-      
-    </>
-  )
-}
-
-ReactDOM.render(
-  <React.StrictMode>
-    <WhoAmI name="John" surname="Smith" link="facebook.com "/>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App/>,document.getElementById('root'));
