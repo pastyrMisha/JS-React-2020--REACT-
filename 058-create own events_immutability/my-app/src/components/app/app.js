9c49d70731851314bfe882035dd2adb04f4d34b4
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-07-22 23:31:54 +0300

    welldone

--- 058-create own events_immutability/my-app/src/components/app/app.js
+++ 058-create own events_immutability/my-app/src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
@@ -7,7 +7,6 @@ import PostList from '../post-list';
 import PostAddForm from '../post-add-form';
 
 import './app.css';
-// import style from './App.module.css';
 import styled from 'styled-components';
 
 const AppBlock = styled.div`
@@ -16,29 +15,74 @@ const AppBlock = styled.div`
 `;
 
 const StyledAppBlock = styled(AppBlock)`
-    background-color: grey;
+    background-color: transparent;
 `
 
-const App = () => {
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data : [
+                {label: 'Going to learn React', important: true, id: '1'},
+                {label: 'That is so good', important: false, id: '2'},
+                {label: 'I need a break...', important: false, id: '3'}
+             ]
+        };
+        this.deleteItem = this.deleteItem.bind(this);
+        this.addItem = this.addItem.bind(this);
 
-     const data = [
-        {label: 'Going to learn React', important: true, id: 'qwqw'},
-        {label: 'That is so good', important: false, id: 'werr'},
-        {label: 'I need a break...', important: false, id: 'fdfdf'}
-     ];
+        this.maxId = 4;
+    }
 
-    return (
-        <StyledAppBlock>
-        {/* <div className={style.app}> */}
-        <AppHeader/>
-        <div className="search-panel d-flex">
-            <SearchPanel/>
-            <PostStatusFilter/>
-        </div>
-        <PostList posts={data} />
-        <PostAddForm/>
-        </StyledAppBlock>
-    )
+deleteItem(id) {
+    this.setState(({data}) => {
+        const index = data.findIndex(elem => elem.id === id)
+        // data.splice(index, 1);
+        // return {
+        //     data: data
+        // } // Меняем data вручную! Это ошибка (!)
+
+        // const before = data.slice(0, index);
+        // const after = data.slice(index + 1);
+        // const newArr = [...before, ...after];
+        // Запишем чище:
+        const newArr = [...data.slice(0, index), ...data.slice(index + 1)];
+
+        //  STATE НАПРЯМУЮ МЫ НЕ ИЗМЕНЯЕМ (!)
+        return {
+            data: newArr 
+        }
+    });
 }
 
-export default App;
+addItem(body) {
+    const newItem = {
+        label: body,
+        important: false,
+        id: this.maxId++
+    }
+    this.setState(({data}) => {
+        const newArr = [...data, newItem];
+        return {
+            data: newArr
+        }
+    });
+}
+
+    render() {
+        return (
+            <StyledAppBlock>
+            <AppHeader/>
+            <div className="search-panel d-flex">
+                <SearchPanel/>
+                <PostStatusFilter/>
+            </div>
+            <PostList 
+                posts={this.state.data} 
+                onDelete={this.deleteItem}/>
+            <PostAddForm
+                onAdd={this.addItem}/>
+            </StyledAppBlock>
+        )
+}
+}
--- 058-create own events_immutability/my-app/src/components/post-list/post-list.js
+++ 058-create own events_immutability/my-app/src/components/post-list/post-list.js
@@ -5,27 +5,21 @@ import {ListGroup} from 'reactstrap';
 
 import './post-list.css';
 
-const PostList = ({posts}) => {
+const PostList = ({posts, onDelete}) => {
 
     const elements = posts.map((item) => {
         const {id, ...itemProps} = item;
         return (
             <li key={id} className='list-group-item'>
-                 {/* <PostListItem
-                  label={item.label} 
-                  important={item.important} />  */}
-                  {/* Новый синтаксис: */}
-                  <PostListItem {...itemProps} />
+                  <PostListItem 
+                  {...itemProps}
+                  onDelete={() => onDelete(id)}/>
             </li>
         )
     })
 
     return (
         <ListGroup className="app-list">
-            {/* Подход неверный (в 0 ячейке переданный может быть и вовсе не объект): */}
-            {/* <PostListItem label={posts[0].label} important={posts[0].important} /> */}
-            {/* <PostListItem label='That is so good' />
-            <PostListItem label='I need a break...' /> */}
             {elements}
         </ListGroup>
     )
