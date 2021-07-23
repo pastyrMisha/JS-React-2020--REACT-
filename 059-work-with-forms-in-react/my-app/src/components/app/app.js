0257bc853475b03438db492b8e9c124e0adebe30
pastyr Misha <skturnercz@gmail.com>
2021-07-23 12:58:40 +0300

    process

--- 059-work-with-forms-in-react/my-app/src/components/app/app.js
+++ 059-work-with-forms-in-react/my-app/src/components/app/app.js
@@ -58,7 +58,18 @@ deleteItem(id) {
 }
 
 onToogleImportant(id) {
-    console.log(`Important ${id}`);
+    this.setState(({data}) => {
+        const index = data.findIndex(elem => elem.id === id);
+
+        const old = data[index];
+        const newItem = {...old, important: !old.important};
+        
+        const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
+
+        return {
+            data: newArr
+        }
+    })
 }
 
 onToogleLiked(id) {
