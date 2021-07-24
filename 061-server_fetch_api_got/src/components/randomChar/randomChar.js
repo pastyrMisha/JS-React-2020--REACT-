29a9d1020e6ea127a01d31646de5d5573ddff43a
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-07-24 22:27:52 +0300

    process

--- 061-server_fetch_api_got/src/components/randomChar/randomChar.js
+++ 061-server_fetch_api_got/src/components/randomChar/randomChar.js
@@ -1,48 +1,73 @@
 import React, {Component} from 'react';
 import './randomChar.css';
 import gotService from '../../services/gotService';
+import Spinner from '../spinner';
 
 export default class RandomChar extends Component {
 
+    constructor() {
+        super();
+        this.updateChar();
+    }
+
     gotService = new gotService();
     state = {
-        name: null,
-        gender: null,
-        born: null,
-        died: null,
-        culture: null
+       char: {},
+       loading: false
+    }
+
+    onCharLoaded = (char) => {
+        this.setState({char})
     }
 
     updateChar() {
-        
+        const id = Math.floor(Math.random()*140 + 25); //25-140
+        this.gotService.getCharacter(id)
+            .then(this.onCharLoaded);
     }
 
     render() {
 
-        const {name, gender, born, died, culture} = this.state;
+        const {char, loading } = this.state;
+
+        const spinner = loading ? <Spinner/> : null;
+        const content = !loading ? <View char={char}/> : null;
 
         return (
             <div className="random-block rounded">
-                <h4>Random Character: {name}</h4>
-                <ul className="list-group list-group-flush">
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Gender </span>
-                        <span>{gender}</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Born </span>
-                        <span>{born}</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Died </span>
-                        <span>{died}</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Culture </span>
-                        <span>{culture}</span>
-                    </li>
-                </ul>
+                {spinner}
+                {content}
             </div>
         );
     }
+}
+
+
+const View = ({char}) => {
+
+    const {name, gender, born, died, culture} = char;
+
+    return (
+        <>
+            <h4>Random Character: {name}</h4>
+            <ul className="list-group list-group-flush">
+                <li className="list-group-item d-flex justify-content-between">
+                    <span className="term">Gender </span>
+                    <span>{gender}</span>
+                </li>
+                <li className="list-group-item d-flex justify-content-between">
+                    <span className="term">Born </span>
+                    <span>{born}</span>
+                </li>
+                <li className="list-group-item d-flex justify-content-between">
+                    <span className="term">Died </span>
+                    <span>{died}</span>
+                </li>
+                <li className="list-group-item d-flex justify-content-between">
+                    <span className="term">Culture </span>
+                    <span>{culture}</span>
+                </li>
+            </ul>    
+        </>
+    )
 }
---
+++ 061-server_fetch_api_got/src/components/spinner/spinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './spinner.css'
+
+const Spinner = () => {
+    return (
+        <div class="loadingio-spinner-spin-ed41cd0fjh"><div class="ldio-wzte8uahlyo">
+        <div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div>
+        </div></div> 
+    )
+}
+
+
+export default Spinner;
