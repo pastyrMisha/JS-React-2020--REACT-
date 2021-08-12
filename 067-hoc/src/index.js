cb025e1a15fe92dc5ccb478e9db412a305f4e658
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-08-12 21:59:09 +0300

    Update index.js

--- 067-hoc/src/index.js
+++ 067-hoc/src/index.js
@@ -6,6 +6,7 @@ import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+
 // class Example extends Component {
 //     render() {
 //         return this.props.children
