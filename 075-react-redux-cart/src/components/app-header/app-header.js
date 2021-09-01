ed5c4c922da8cc4bca61f5fecb7a48a88dc801c4
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-09-01 22:45:35 +0300

    welldone

--- 075-react-redux-cart/src/components/app-header/app-header.js
+++ 075-react-redux-cart/src/components/app-header/app-header.js
@@ -4,30 +4,25 @@ import './app-header.scss';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 
-const AppHeader = () => {
-    // let total = 0;
-
-    // for(let i = 0; i< items.length; i++){
-    //     total = total + items[i];
-    // }
+const AppHeader = ({total}) => {
+ 
 
     return (
         <header className="header">
             <Link to = {'/'} className="header__link">Menu</Link>
             <Link to = "/cart" className="header__link">
                 <img className="header__cart" src={cartIcon} alt="cart"></img>
-                Total: {0} $
+                Total: {total} $
             </Link>
         </header>
     )
 };
 
 
-const mapStateToProps = (state) => {
+const mapStateToProps = ({total}) => {
     return {
-        menuItems: state.menu,
-        loading: state.loading
+        total
     }
 }
 
-export default connect(({total}) => {() => total})(AppHeader);
+export default connect(mapStateToProps)(AppHeader);
